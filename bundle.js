4a293d476e73611976d25c76e1b2f44ba687c652
gaboesquivel <contact@gaboesquivel.com>
2016-12-10 15:25:40 -0600

    add gaboesquivel pkg

--- bundle.js
+++ bundle.js
@@ -1,50 +1,73 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const asistentes = [];
+	asistentes.push(__webpack_require__(1));
+	
+	const printname = persona => {
+	  console.log(persona.name);
+	};
+	
+	asistentes.forEach(printname);
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
-	eval("console.log('hello');//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9pbmRleC5qcz8yNjQ1Il0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiJBQUFBQSxRQUFRQyxHQUFSLENBQVksT0FBWiIsImZpbGUiOiIwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ2hlbGxvJyk7XG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9pbmRleC5qcyJdLCJzb3VyY2VSb290IjoiIn0=");
+	module.exports = {
+		"name": "Gabo Esquivel",
+		"bio": "Software Developer and Consultant. Community leader and open source contributor. Organizer of tech meetups and NodeSchool mentor.",
+		"website": "http://gaboesquivel.com",
+		"email": "contact@gaboesquivel.com",
+		"twitter": "gaboesquivel",
+		"github": "gaboesquivel",
+		"linkedin": "gaboesquivel",
+		"ama": "https://github.com/gaboesquivel/ama"
+	};
 
 /***/ }
 /******/ ]);
+//# sourceMappingURL=bundle.js.map
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const asistentes = []
+asistentes.push(require('gaboesquivel'))
+
+const printname = (persona) => {
+  console.log(persona.name);
+}
+
+asistentes.forEach(printname);
