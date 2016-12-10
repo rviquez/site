8eaa0850c733b959ff57ad86c720384a6dd50f9c
Jeremias Ceciliano <jecel0911@gmail.com>
2016-12-10 15:36:34 -0600

    add jeremiasceciliano module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const asistentes = []
 asistentes.push(require('gaboesquivel'))
 asistentes.push(require('laubits'))
+asistentes.push(require('jeremiasceciliano'))
 
 const printname = (persona) => {
   console.log(persona.name);
